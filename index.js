65d7e97f2ca0d3e5fdf0201479a67a20dbb43f01
abhisheksingh@aviansoft.in <abhisheksinghaviansoft@gmail.com>
2025-11-23 21:33:13 +0530

    index.js

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = 5000
-
-
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
+
 app.get('/login',(req,res)=>{
-    res.send('<h1>Hello world </h1>')
+    // res.send('<h1>Hello world </h1>')
+    const jokes = [
+        {
+        id:1,
+        title: 'A jokes',
+        content:'this is a jokes'
+    },
+    {
+        id:2,
+        title: 'B jokes',
+        content:'this is a jokes'
+    }
+
+];
+res.send(jokes)
 })
 
 app.get('/twitter',(req,res)=>{
